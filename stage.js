e1288e044b10d809d48dc21f429696227555b8a3
sikaikzari <zoukun5@yahoo.co.jp>
2026-04-04 08:37:12 +0900

    Add files via upload

--- stage.js
+++ stage.js
@@ -51,7 +51,7 @@ function genStage1() {
       coinArr.push({ x: bx + 8 + c * 32, y: by - 30, col: false, ph: Math.random() * Math.PI * 2 });
     }
     // よちよち敵（地面歩き）
-    if (i % 3 === 1) {
+    if (i % 5 === 2) {
       enms.push({ x: bx + 30, y: GND() - 28, w: 26, h: 26, vx: -(0.4 + Math.random() * 0.3), vy: 0, alive: true, flying: false, spiky: false, type: 'yochi', knockvx: 0, knockvy: 0 });
     }
     // ゴースト（空中浮遊・触ると痛い）
@@ -96,7 +96,7 @@ function genStage2() {
     }
   });
 
-  plats.push({ x: 1600, y: GND() - 60, w: 20, h: 60, t: 'checkpoint' });
+plats.push({ x: 1450, y: GND(), w: 20, h: 60, t: 'checkpoint' });
 }
 
 function genStage3() {
